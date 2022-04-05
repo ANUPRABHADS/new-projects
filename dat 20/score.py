13b3c8f89ac37752192f21c95ea481b1f091041f
Anuprabha D S <anuprabhads@gmail.com>
2022-04-05 15:37:18 +0530

    Snake game modified

--- dat 20/score.py
+++ dat 20/score.py
@@ -1,12 +1,15 @@
-from  turtle import Turtle
+from turtle import Turtle
+
+
 class Score(Turtle):
     def __init__(self):
         super().__init__()
-        self.score=0
-        self.high_score=0
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.hideturtle()
         self.penup()
-        self.goto(0,220)
+        self.goto(0, 220)
         self.update()
 
     def score_add(self):
@@ -15,28 +18,12 @@ class Score(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
-            self.high_score=self.score
-        self.score=0
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
         self.update()
-
-    # def game_over(self):
-    #     self.goto(0,0)
-    #     self.write("GAME OVER", align='center', font=('Arial', 22, 'normal'))
 
     def update(self):
         self.clear()
         self.write(f"Score = {self.score} High Score: {self.high_score}", align='center', font=('Arial', 22, 'normal'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
